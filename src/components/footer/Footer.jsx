81cb134154a533fcd01e349e86e91039b87a9693
arfat_kazi <arfatkazi115@gmail.com>
2024-10-14 17:14:36 +0530

    creating footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,17 +4,41 @@ const Footer = () => {
   return (
     <>
       <div className="footer ">
-        <h1>footer</h1>
-        <h1>footer</h1>
-        <h1>footer</h1>
-        <h1>footer</h1>
-        <h1>footer</h1>
-        <h1>footer</h1>
-        <h1>footer</h1>
-        <h1>footer</h1>
-        <h1>footer</h1>
-        <h1>footer</h1>
-        <h1>footer</h1>
+        <div className="section-1 swipe-section">
+          <div className="cards">
+            <h1 className="card-name">arfat kazi</h1>
+            <p className="card-para">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+              omnis atque, libero nam hic possimus, itaque dolorem repellendus
+              blanditiis earum deleniti esse inventore necessitatibus ratione.
+              Enim doloribus error nam fuga.
+            </p>
+          </div>
+
+          {/* card-2 */}
+          <div className="cards">
+            <h1 className="card-name">arfat kazi</h1>
+            <p className="card-para">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+              omnis atque, libero nam hic possimus, itaque dolorem repellendus
+              blanditiis earum deleniti esse inventore necessitatibus ratione.
+              Enim doloribus error nam fuga.
+            </p>
+          </div>
+          {/* end of card-2 */}
+
+          {/* card-3 */}
+          <div className="cards">
+            <h1 className="card-name">arfat kazi</h1>
+            <p className="card-para">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+              omnis atque, libero nam hic possimus, itaque dolorem repellendus
+              blanditiis earum deleniti esse inventore necessitatibus ratione.
+              Enim doloribus error nam fuga.
+            </p>
+          </div>
+          {/* end of card-3 */}
+        </div>
       </div>
     </>
   );
