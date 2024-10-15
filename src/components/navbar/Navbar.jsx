d3710387106e56e25c03439bdfedee2aad234210
arfat_kazi <arfatkazi115@gmail.com>
2024-10-15 14:05:58 +0530

    changes into nacbar again

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,25 @@
+import { useState } from "react";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [isProductOpen, setIsProductOpen] = useState(false);
+  const [isSafetyOpen, setIsSafetyOpen] = useState(false);
+
+  const mouseEnterHandler = () => {
+    setIsProductOpen(true);
+  };
+
+  const mouseLeaveHandler = () => {
+    setIsProductOpen(false);
+  };
+
+  const safetyHandler = () => {
+    setIsSafetyOpen(true);
+  };
+
+  const safetyleaveHandler = () => {
+    setIsSafetyOpen(false);
+  };
   return (
     <>
       <div className="navbar text-white ">
@@ -12,54 +31,63 @@ const Navbar = () => {
           />
           <h1>tinder</h1>
           <ul className="nav-list">
-            <li className="product-list">
+            <li
+              className="product-list"
+              onMouseEnter={mouseEnterHandler}
+              onMouseLeave={mouseLeaveHandler}
+            >
               <a className="products">products</a>
-              <h5>
-                <span>premium features</span>
-              </h5>
-              <h5>
-                <span>subscription tiers</span>
-              </h5>
-
-              <h5>
-                <span>tinder gold</span>
-              </h5>
-
-              <h5>
-                <span>tinder platinum</span>
-              </h5>
-
-              <h5>
-                <span>tinder plus</span>
-              </h5>
-              <h5>
-                <span>tinder select</span>
-              </h5>
+              {isProductOpen && (
+                <div className="dropdown">
+                  <h5>
+                    <span>premium features</span>
+                  </h5>
+                  <h5>
+                    <span>subscription tiers</span>
+                  </h5>
+                  <h5>
+                    <span>tinder gold</span>
+                  </h5>
+                  <h5>
+                    <span>tinder platinum</span>
+                  </h5>
+                  <h5>
+                    <span>tinder plus</span>
+                  </h5>
+                  <h5>
+                    <span>tinder select</span>
+                  </h5>
+                </div>
+              )}
             </li>
             <li>
               <a>learn</a>
             </li>
-            <li>
+            <li
+              className="safety-list"
+              onMouseEnter={safetyHandler}
+              onMouseLeave={safetyleaveHandler}
+            >
               <a>safety</a>
-
-              <h5>
-                <span>community guidelines</span>
-              </h5>
-              <h5>
-                <span>safety tips</span>
-              </h5>
-
-              <h5>
-                <span>safety and policies</span>
-              </h5>
-
-              <h5>
-                <span>safety and reporting</span>
-              </h5>
-
-              <h5>
-                <span>security</span>
-              </h5>
+              {isSafetyOpen && (
+                <div className="dropdown">
+                  <h5>
+                    <span>community guidelines</span>
+                  </h5>
+                  <h5>
+                    <span>safety tips</span>
+                  </h5>
+                  <h5>
+                    <span>safety and policies</span>
+                  </h5>
+                  <h5>
+                    <span>safety and reporting</span>
+                  </h5>
+                  <h5>
+                    <span>security</span>
+                  </h5>
+                </div>
+              )}
             </li>
             <li>
               <a className="support">support</a>
