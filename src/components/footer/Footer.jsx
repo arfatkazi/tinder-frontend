a45c28e5095d0b3df132ca4284d49420f96cd64a
arfat_kazi <arfatkazi115@gmail.com>
2024-10-17 17:49:27 +0530

    creating page 3 slide card

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,41 +3,9 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <>
-      <div className="footer ">
-        <div className="section-1 swipe-section">
-          <div className="cards">
-            <h1 className="card-name">arfat kazi</h1>
-            <p className="card-para">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
-              omnis atque, libero nam hic possimus, itaque dolorem repellendus
-              blanditiis earum deleniti esse inventore necessitatibus ratione.
-              Enim doloribus error nam fuga.
-            </p>
-          </div>
-
-          {/* card-2 */}
-          <div className="cards">
-            <h1 className="card-name">arfat kazi</h1>
-            <p className="card-para">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
-              omnis atque, libero nam hic possimus, itaque dolorem repellendus
-              blanditiis earum deleniti esse inventore necessitatibus ratione.
-              Enim doloribus error nam fuga.
-            </p>
-          </div>
-          {/* end of card-2 */}
-
-          {/* card-3 */}
-          <div className="cards">
-            <h1 className="card-name">arfat kazi</h1>
-            <p className="card-para">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
-              omnis atque, libero nam hic possimus, itaque dolorem repellendus
-              blanditiis earum deleniti esse inventore necessitatibus ratione.
-              Enim doloribus error nam fuga.
-            </p>
-          </div>
-          {/* end of card-3 */}
+      <div id="page3">
+        <div id="moving-cards">
+          <div id="card"></div>
         </div>
       </div>
     </>
